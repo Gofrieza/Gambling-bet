6ae7d6da5c4de8c2c652059311a9d966f627e7e2
Gofrieza <93874332+Gofrieza@users.noreply.github.com>
2022-01-31 19:33:09 +0530

    Add files via upload

---
+++ UC2-WinOrLoss.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace casino
+{
+    internal class UC2_WinOrLoss
+    {
+        public static void winOrloss()
+        {
+            double totalMoney = 100.00;
+            double betMoney = 1.00;
+            Random random = new Random();
+            int Check = random.Next(0, 2);
+            if (Check == 1)
+            {
+                Console.WriteLine("win");
+                Console.ReadKey();
+            }
+            else
+            {
+                Console.WriteLine("win" );
+                Console.ReadKey();
+            }
+        }
+    }
+}
