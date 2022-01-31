6d6af87bff8f5e2f79677706fd6409bf4415aa6f
Gofrieza <93874332+Gofrieza@users.noreply.github.com>
2022-01-31 19:11:17 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+using casino;
+
+Console.WriteLine("Hello, World!");
+UC1_gambler.bet();
---
+++ UC1_gambler.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace casino
+{
+    internal class UC1_gambler
+    {
+        public static void bet()
+        {
+            double totalMoney = 100.00;
+            double betMoney = 1.00;
+        }
+    }
+}
